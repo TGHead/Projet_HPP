e6e314efecb9c857e3ee86fde292fea08b5b157a
Haoming ZHAN <haoming.zhan@telecom-st-etienne.fr>
2017-04-28 14:20:47 +0200

    basic test added

---
+++ src/test/java/team/tse/hpp/TestBasic.java
@@ -0,0 +1,17 @@
+package team.tse.hpp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/4/28.
+ */
+public class TestBasic {
+    @Test
+    public void test() {
+        //TODO: Q1Basic test
+        fail("write test!");
+    }
+
+}
