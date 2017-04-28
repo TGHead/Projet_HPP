0524bbb57580782da16e06f03b3d7a2a7f261f66
Haoming ZHAN <haoming.zhan@telecom-st-etienne.fr>
2017-04-28 11:24:38 +0200

    Class structure modified

---
+++ src/main/java/team/tse/hpp/Item.java
@@ -0,0 +1,31 @@
+package team.tse.hpp;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017/4/28.
+ */
+
+public interface Item {
+
+    Date getTs_();
+
+    int getId_();
+
+    int getUser_id_();
+
+    String getContenu_();
+
+    String getUser_();
+
+    int getScore_();
+
+    int getCommenters_();
+
+    void scoreDecrement();
+
+    int getSumScore();
+
+    boolean IsMyComment(Comment comment);
+
+}
--- src/main/java/team/tse/hpp/Post.java
+++ src/main/java/team/tse/hpp/Post.java
@@ -7,50 +7,63 @@ import java.util.Date;
  * Created by TGHead on 2017/4/27.
  */
 
-public class Post {
+public class Post implements Item {
 
+    protected int score_;// record the score of this post
+    protected ArrayList<Comment> liste_c;
     private Date ts_;
-    private int post_id_, user_id_;
-    private String post_, user_;
-
-    private int score_;// record the score of this post
+    private int id_, user_id_;
+    private String contenu_, user_;
     private int commenters_;// record the number of commenters (excluding the post author) for the post
-
-    private ArrayList<Comment> liste_c;
 
     public Post(String ts, String post_id, String user_id, String post, String user) {
         this.ts_ = new Date(Long.parseLong(ts));
-        this.post_id_ = Integer.parseInt(post_id);
+        this.id_ = Integer.parseInt(post_id);
         this.user_id_ = Integer.parseInt(user_id);
-        this.post_ = post;
+        this.contenu_ = post;
         this.user_ = user;
 
         this.score_ = 10;
         this.commenters_ = 0;
-
         this.liste_c = new ArrayList<Comment>();
     }
 
+    @Override
     public Date getTs_() {
         return ts_;
     }
 
-    public int getPost_id_() {
-        return post_id_;
+    @Override
+    public int getId_() {
+        return id_;
     }
 
+    @Override
+    public int getUser_id_() {
+        return user_id_;
+    }
+
+    @Override
+    public String getContenu_() {
+        return contenu_;
+    }
+
+    @Override
     public String getUser_() {
         return user_;
     }
 
+    @Override
     public int getScore_() {
         return score_;
     }
 
+    @Override
     public int getCommenters_() {
         return commenters_;
     }
 
+    @Override
     public void scoreDecrement() {
         if (this.score_ > 0)
             this.score_--;
@@ -59,6 +72,7 @@ public class Post {
         }
     }
 
+    @Override
     public int getSumScore() {
         int sum = this.score_;
         for (Comment c : liste_c) {
@@ -67,10 +81,21 @@ public class Post {
         return sum;
     }
 
-    public void commentersUpdate(Comment comment) {
+    @Override
+    public boolean IsMyComment(Comment comment) {
+        if (comment.getPost_commented_() == this.id_ || comment.getComment_replied_() == this.id_)
+            return true;
         for (Comment c : liste_c) {
-            if (!c.IsMyUser(comment))
-                this.commenters_++;
+            if (c.IsMyComment(comment))
+                return true;
         }
+        return false;
     }
+
+//    public void commentersUpdate(Comment comment) {
+//        for (Comment c : liste_c) {
+//            if (!c.IsMyUser(comment))
+//                this.commenters_++;
+//        }
+//    }
 }
